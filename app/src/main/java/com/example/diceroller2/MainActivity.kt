bea44ab713e5c5f8c3e7ff69b4d4937bd45243d4
marevandro <marevandro95@gmail.com>
2024-06-08 19:09:17 -0300

    dice roller

---
+++ app/src/main/java/com/example/diceroller2/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.diceroller2
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener{
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val randomInt = java.util.Random().nextInt(6) + 1
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+
+    }
+}
